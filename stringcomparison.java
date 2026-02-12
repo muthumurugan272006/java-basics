b26c61e32e3de3fcdff029ca2231dbda836d9416
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-12 22:28:48 +0530

    added

---
+++ stringcomparison.java
@@ -0,0 +1,13 @@
+// to compare string we need to use .equals()
+public class stringcomparison {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "hello";
+        if (a.equals(b)) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+
+}
