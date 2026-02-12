9b0802bc4bbd2912a42fb622a18adef2c62557b3
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-12 22:25:32 +0530

    added

---
+++ comparison.java
@@ -0,0 +1,15 @@
+// comparison operator => used to compare two values  > < 
+// to check given numbers are equal use == operator
+public class comparison {
+    public static void main(String[] args) {
+            int a =10;
+    int b =20;
+    if(a>b)
+    {
+        System.out.println("a is greater");
+    }else{
+        System.out.println("b is greater");
+    }
+    }
+
+}
