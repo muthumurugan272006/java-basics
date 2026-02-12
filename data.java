9dbfdfce51b555560e9a82f010c5b7b437b475a6
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-12 22:03:55 +0530

    added

---
+++ data.java
@@ -0,0 +1,19 @@
+// variables and data types
+public class data {
+    public static void main(String[]args){
+        //premitive data type=> stores actual value eg: int a = 10; here a directly stores the value
+        int a =10;// to store integer values
+        byte b =8;
+        short c=7;
+        long d=3234324;
+        float e = 12.8f;//=> to use float we need to use f 
+        double f = 32.76;
+        char g = 'f';//=> used to store only one character
+        boolean h = true; // true or false
+        // non - premitive data type=>stores reference (address) of the object eg: String a ="Hello"; here a stored in stack, Hello stores in heap
+        String i="Hello";
+        int []arr={1,2,3,4,5};
+        // class
+        //interface
+    }
+}
