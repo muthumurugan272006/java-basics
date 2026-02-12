c2b35d02ea92d00d721b432239514c9dd5c0cd0e
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-12 21:50:27 +0530

    added

--- format.java
+++ format.java
@@ -1,7 +1,14 @@
 // Structure of java program
 public class format {
-    public static void main(String[]args)
+    public static void main(String[]args)// Entry point of the java program
     {
         System.out.println("Hello World");
     }
 }
+/*
+ public static void main(String[]args)
+ public => accessible from anywhere
+ static => used for accessible
+ void=> does not return any type of data
+ String[] args=>used to recive command line argument
+ */
