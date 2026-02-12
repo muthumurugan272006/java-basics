efe4e4173ace913532681282398dfecd62f685c7
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-12 22:15:36 +0530

    added

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+public class ifelse {
+    public static void main(String[] args) {
+        /*
+         * syntax for if else
+         * if(condition){
+         * condition to be executed
+         * }else{
+         * condition to be executed
+         * }
+         */
+        boolean rain = true;
+        if (rain) {
+            System.out.println("Take an umbrella");
+        } else {
+            System.out.println("Enjoy your day");
+        }
+    }
+}
