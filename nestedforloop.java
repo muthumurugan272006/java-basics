511086985ee7d720cc6ff525f68b2318f0edcb91
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-14 19:15:45 +0530

    added

--- nestedforloop.java
+++ nestedforloop.java
@@ -5,9 +5,9 @@ Inner loop runs completely
 */
 public class nestedforloop {
     public static void main(String[] args) {
-        for(int i=1;i<5;i=i+1)
+        for(int i=1;i<=5;i=i+1)// no of rows
         {
-            for(int j=1;j<i;j=j+1)
+            for(int j=1;j<=i;j=j+1)// no of starts
             {
                 System.out.println("*");
             }
