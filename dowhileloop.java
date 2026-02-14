63430c7918e7433f68f3eddabe03d21c8d8ccb55
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-14 18:12:34 +0530

    added

---
+++ dowhileloop.java
@@ -0,0 +1,12 @@
+/*A do-while loop is similar to a while loop, but with one important difference:
+ It executes the block at least once, even if the condition is false.
+ */
+public class dowhileloop {
+    public static void main(String[] args) {
+        /*syntax
+        do {
+    // code to execute
+    } while (condition);
+        */   
+    }
+}
