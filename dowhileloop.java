91be221fc4e7703571d7e5070b59dedcab0f2899
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-14 18:30:27 +0530

    added

--- dowhileloop.java
+++ dowhileloop.java
@@ -1,6 +1,7 @@
 /*A do-while loop is similar to a while loop, but with one important difference:
  It executes the block at least once, even if the condition is false.
  */
+import java.util.Scanner;
 public class dowhileloop {
     public static void main(String[] args) {
         /*syntax
@@ -8,5 +9,11 @@ public class dowhileloop {
     // code to execute
     } while (condition);
         */   
+        Scanner scan = new Scanner(System.in);
+        int count =0;
+        do{
+            System.out.print("enter the number greater than 18");
+            count = scan.nextInt();
+        }while(count<18);
     }
 }
