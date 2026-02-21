390d3d6b3b570af29e667dea5ed84d69befd3d80
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-21 16:45:45 +0530

    added

---
+++ foreachloop.java
@@ -0,0 +1,8 @@
+public class foreachloop {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,6};
+        for(int var:arr){
+            System.out.println(var);
+        }
+    }
+}
