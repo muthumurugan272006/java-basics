f4f30f02d7a4d6938a05d86c75c8188ece55a96c
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-24 19:42:22 +0530

    added

---
+++ statickeyword.java
@@ -0,0 +1,12 @@
+//The final keyword in Java is used to restrict modification.
+class hi{
+    
+}
+public class statickeyword {
+    public static void main(String[] args) {
+//1. Final Variable (Constant)
+//Once a value is assigned, it cannot be changed.
+final int a =10;
+a=20;// this shows error because if we declared as finall we cannot able to modify        
+    }
+}
