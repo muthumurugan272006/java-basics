becd385bd37cfba03fb30ffb3b706a83aa34d3eb
muthumurugan27 <vh13269_ece23@velhightech.com>
2026-02-24 20:05:59 +0530

    added

---
+++ interfacee.java
@@ -0,0 +1,13 @@
+interface animal{
+    void sound();
+}
+class a implements animal{// while using interface we need to use implements
+    public void sound(){// remember that while using interface we need to use public 
+        System.out.println("bark");
+    }
+}
+public class interfacee {
+    public static void main(String[] args) {
+        
+    }
+}
